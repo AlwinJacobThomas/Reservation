fed6e66a5642512be8942a8adaec20e99a015bb1
AlwinJacobThomas <kailath.ajt@gmail.com>
2024-03-19 12:15:33 +0530

    added models of Users,MenuTable,Booking,connected mysql

--- reservation/reservationApp/admin.py
+++ reservation/reservationApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import MenuTable,Booking,Users
 # Register your models here.
+admin.site.register(Users)
+admin.site.register(MenuTable)
+admin.site.register(Booking)
--- reservation/reservationApp/models.py
+++ reservation/reservationApp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+    email = models.EmailField(max_length=30)
+    password = models.CharField(max_length=50)
+
+class Booking(models.Model):
+    name = models.CharField(max_length=50)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateTimeField()
+    
+class MenuTable(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+    
+
+    
